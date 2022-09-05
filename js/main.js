f250bf42f93152c62cb29e50219728fad1cffd59
GuilhermeCostaDF <guidocostadf@gmail.com>
2022-09-05 19:30:03 -0300

    criando as funçoes de soma e subtração dos poderes

--- js/main.js
+++ js/main.js
@@ -1,3 +1,20 @@
-document.querySelector('.robo').addEventListener('click', () =>{
-    console.log('oi')
-})
+const subtrair = document.querySelector('#subtrair');
+const somar = document.querySelector('#somar');
+const braco = document.querySelector('#braco');
+
+const controle = document.querySelectorAll('.controle-ajuste');
+
+
+somar.addEventListener('click', () => manipulaDados("somar"))
+
+subtrair.addEventListener('click', () => manipulaDados("subtrair"))
+
+function manipulaDados(operacao) {
+    if (operacao == "subtrair") {
+        braco.value = parseInt(braco.value) - 1;
+    } else {
+        braco.value = parseInt(braco.value) + 1;
+    }
+}
+
+console.log(controle)
