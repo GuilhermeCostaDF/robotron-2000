7f893b7f25f0b5137bd094856301e2def343e364
GuilhermeCostaDF <guidocostadf@gmail.com>
2022-09-05 19:39:18 -0300

    refatorando o codigo para alterar varios elementos

--- js/main.js
+++ js/main.js
@@ -4,17 +4,15 @@ const braco = document.querySelector('#braco');
 
 const controle = document.querySelectorAll('.controle-ajuste');
 
+controle.forEach((elemento) => {
+    elemento.addEventListener('click', (event) => manipulaDados(event.target.textContent))
+})
 
-somar.addEventListener('click', () => manipulaDados("somar"))
-
-subtrair.addEventListener('click', () => manipulaDados("subtrair"))
 
 function manipulaDados(operacao) {
-    if (operacao == "subtrair") {
+    if (operacao == "-") {
         braco.value = parseInt(braco.value) - 1;
     } else {
         braco.value = parseInt(braco.value) + 1;
     }
 }
-
-console.log(controle)
